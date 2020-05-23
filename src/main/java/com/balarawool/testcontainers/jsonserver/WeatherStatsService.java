fb4e240b8814319c408e29d3a4e4223a8e2f91a9
Balkrishna <balkrishna.rawool@gmail.com>
2020-05-23 13:36:13 +0200

    initial version

---
+++ src/main/java/com/balarawool/testcontainers/jsonserver/WeatherStatsService.java
@@ -0,0 +1,49 @@
+package com.balarawool.testcontainers.jsonserver;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Service
+public class WeatherStatsService {
+
+    @Value("${weatherservice.host}")
+    private String host;
+    @Value("${weatherservice.port}")
+    private String port;
+
+    private final RestTemplate restTemplate = new RestTemplateBuilder().build();
+
+    public WeatherStats getWeatherStats() {
+        final String url = "http://" + host + ":" + port + "/forecast24hours";
+
+        DayForecast dayForecast = restTemplate.getForEntity(url, DayForecast.class).getBody();
+        final int max = dayForecast.getForecast().stream().map(hf -> hf.getTemperature()).max(Integer::compare).get();
+        final int min = dayForecast.getForecast().stream().map(hf -> hf.getTemperature()).min(Integer::compare).get();
+
+        return new WeatherStats(max, min);
+    }
+
+    @Data
+    static class DayForecast {
+        private List<HourForecast> forecast;
+    }
+
+    @Data
+    static class HourForecast {
+        private int offset;
+        private int temperature;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class WeatherStats {
+        private int max;
+        private int min;
+    }
+}
---
+++ src/test/java/com/balarawool/testcontainers/jsonserver/WeatherStatsServiceIntegrationTest.java
@@ -0,0 +1,66 @@
+package com.balarawool.testcontainers.jsonserver;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.containers.wait.strategy.WaitAllStrategy;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+        classes = WeatherStatsServiceIntegrationTest.TestConfiguration.class
+)
+public class WeatherStatsServiceIntegrationTest {
+
+    private static GenericContainer jsonServer = new GenericContainer("clue/json-server")
+            .withClasspathResourceMapping("db.json","/data/db.json", BindMode.READ_WRITE)
+            .withExposedPorts(80)
+            .waitingFor(getWaitStrategy());
+
+    private static WaitAllStrategy getWaitStrategy() {
+        return new WaitAllStrategy().withStrategy(Wait.forHttp("/").forStatusCode(200));
+    }
+
+    @DynamicPropertySource
+    public static void setPostManagerApiConfig(DynamicPropertyRegistry registry) {
+        jsonServer.start();
+
+        registry.add("weatherservice.host", jsonServer::getContainerIpAddress);
+        registry.add("weatherservice.port", jsonServer::getFirstMappedPort);
+    }
+
+    @Autowired
+    private WeatherStatsService weatherStatsService;
+
+    @Test
+    public void shouldGetStubbedWeatherData() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+
+        WeatherStatsService.WeatherStats weatherStats = weatherStatsService.getWeatherStats();
+
+        assertEquals(32, weatherStats.getMax());
+        assertEquals(25, weatherStats.getMin());
+    }
+
+    @EnableAutoConfiguration // Important because testcontainers does some auto-configuration for GenericContainer
+    @ComponentScan({"com.balarawool.testcontainers.jsonserver"})
+    @Configuration
+    static class TestConfiguration {
+    }
+
+}
